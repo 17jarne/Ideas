1ec1827327704ea5a92331f72317439ac7791e96
root <root@localhost.localdomain>
2017-03-27 18:59:16 +0200

    updated the parts of the brain + style

--- neuralideas/amygdala_cadvanced.py
+++ neuralideas/amygdala_cadvanced.py
@@ -5,22 +5,24 @@ import numpy as np
 from neuron import Neuron
 
 actthreshold = 0.5
-class amygdala_c(object):
-
-        def __init__(self, size):
-                #create neurons
-                preneurons = [Neuron(0, 'proneuron', actthreshold) for i in range(size)]
-                i = 0
-                for neuron in preneurons:
-                        neuron.changename(i)
-                        i += 1
-                self.neurons = preneurons
-                self.size = size
-                #connect neurons
-                defaultstrength = 0.4
-                for neuron1 in neurons:
-                        for neuron2 in neurons:
-                                if neuron1 is not neuron2:
-                                        neuron1.add_inconnects([neuron2, defaultweight])
-                                        neuron2.add_outconnects([neuron1, defaultweight])
+class AmygdalaC(object):
+    def __init__(self, size):
+        preneurons = []
+        defaultweight = 0.3
+        #create neurons
+        for i in range(size):
+            tempneuron = Neuron(0, 'proneurons', actthreshold)
+            preneurons.append(tempneuron)
+        i = 0
+        for neuron in preneurons:
+            neuron.changename(i)
+            i += 1
+        self.neurons = preneurons
+        self.size = size
+        #connect neurons
+        for neuron1 in self.neurons:
+            for neuron2 in self.neurons:
+                if neuron1 is not neuron2:
+                    neuron1.add_inconnects([neuron2, defaultweight])
+                    neuron2.add_outconnects([neuron1, defaultweight])
 
--- neuralideas/sensory_cadvanced.py
+++ neuralideas/sensory_cadvanced.py
@@ -5,26 +5,24 @@ import numpy as np
 from neuron import Neuron
 
 actthreshold = 0.5
-class sensory_c:
-	def __init__(self, size):
-                preneurons = []
-		smell1 = Neuron(0, 'inputneuron', actthreshold)
-                defaultweight = 0.3
-		#create neurons
-		for i in range(size):
-			tempneuron = Neuron(0, 'inputneuron', actthreshold)
-			preneurons.append(tempneuron)
-		i = 0
-		for neuron in preneurons:
-			neuron.changename(i)
-			i += 1
-		self.neurons = preneurons
-		self.size = size
-		#connect neurons
-		defaultstrength = 0.4
-		for neuron1 in self.neurons:
-			for neuron2 in self.neurons:
-				if neuron1 is not neuron2:
-					neuron1.add_inconnects([neuron2, defaultweight])
-                                        neuron2.add_outconnects([neuron1, defaultweight])
+class SensoryC:
+    def __init__(self, size):
+        preneurons = []
+        defaultweight = 0.3
+        #create neurons
+        for i in range(size):
+            tempneuron = Neuron(0, 'inputneuron', actthreshold)
+            preneurons.append(tempneuron)
+        i = 0
+        for neuron in preneurons:
+            neuron.changename(i)
+            i += 1
+        self.neurons = preneurons
+        self.size = size
+        #connect neurons
+        for neuron1 in self.neurons:
+            for neuron2 in self.neurons:
+                if neuron1 is not neuron2:
+                    neuron1.add_inconnects([neuron2, defaultweight])
+                    neuron2.add_outconnects([neuron1, defaultweight])
 
